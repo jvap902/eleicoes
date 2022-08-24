120209b2e6c1676cf8d67720578c43ad1d0b238d
juliaconzatti <juliarconzatti@gmail.com>
2022-08-24 09:03:01 -0300

    oi

---
+++ resources/views/base/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+@include('base.header', ['title' => 'Eleições'])
+<body>
+    <div class="container my-5">
+        @yield('container')
+    </div>
+
+    @include('base.footer')
+
+</body>
+</html>
