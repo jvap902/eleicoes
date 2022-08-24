3a851503e1856fcee1a81ff712fed0261c68ea98
juliaconzatti <juliarconzatti@gmail.com>
2022-08-24 09:18:52 -0300

    iiioooi

---
+++ resources/views/eleitores/index.blade.php
@@ -0,0 +1,6 @@
+@extends('base.index')
+
+@section('container')
+<a class="btn btn-dark d-grid gap-2 col-6 mx-auto my-3" href="/eleitores/create">Novo cadastro</a>
+<a class="btn btn-dark d-grid gap-2 col-6 mx-auto my-3" href="/home/">Voltar</a>
+@endsection
