5efde0669bb984379cb99267c040887b6f84e3b5
Alyssa Michalski <alyssa.070505@bento.local>
2022-08-30 16:54:01 -0300

    estilo home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,10 +1,10 @@
 
 
 @extends('base.index')
-<div class="mx-auto" style="width: 40%;">
+<div class="mx-auto" style="width: 40%;" id="btns">
 
-        <button class="btn">Eleitores</button>
-
+    <a href="/eleitores"><button class="btn">Eleitores</button><a>
+    <a href="/votos"><button class="btn">Votos</button><a>
 </div>
 
 @section('container')
