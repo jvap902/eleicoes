a90f63e0696a7192c2d57f747c1013a3338eae49
jvtrindade <104903503+jvtrindade@users.noreply.github.com>
2022-08-31 08:51:33 -0300

    candidatos create

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,6 +5,7 @@
 
     <a href="/eleitores"><button class="btn">Eleitores</button><a>
     <a href="/votos"><button class="btn">Votos</button><a>
+    <a href="/candidatos"><button class="btn">Candidatos</button><a>
 </div>
 
 @section('container')
