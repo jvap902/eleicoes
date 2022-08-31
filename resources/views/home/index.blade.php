310b57b1ad174fc47178d13f7a2cd2c97d040d7c
jvtrindade <104903503+jvtrindade@users.noreply.github.com>
2022-08-31 08:59:48 -0300

    botoes home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,9 +3,9 @@
 @extends('base.index')
 <div class="mx-auto" style="width: 40%;" id="btns">
 
-    <a href="/votos"><button class="btn">Votos</button><a>
-    <a href="/eleitores"><button class="btn">Eleitores</button><a>
-    <a href="/candidatos"><button class="btn">Candidatos</button><a>
+    <a href="/votos"><button class="btn" id="btn">Votos</button><a>
+    <a href="/eleitores"><button class="btn" id="btn">Eleitores</button><a>
+    <a href="/candidatos"><button class="btn" id="btn">Candidatos</button><a>
     <a href="/periodos"><button class="btn" id="btn">Periodos</button><a>
 </div>
 
