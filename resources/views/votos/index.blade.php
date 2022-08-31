bda6ca5ea49cd2bbff23b99ffe4fd49b7a00ff41
Alyssa Michalski <alyssa.070505@bento.local>
2022-08-31 09:19:37 -0300

    verificacao para voto

--- resources/views/votos/index.blade.php
+++ resources/views/votos/index.blade.php
@@ -4,8 +4,6 @@
 
     <!-- <a href="/votos/create"><button class="btn" id="btn">Votar</button><a> -->
     <a href="/votos/titulo"><button class="btn" id="btn">Votar</button><a>
-
-    <a href="/votos/create" style="text-decoration:none"><button class="btn d-grid gap-2 col-6 mx-auto" id="btn">Votar</button><a>
     <a href="/" style="text-decoration:none"><button class="btn d-grid gap-2 col-6 mx-auto" id="btn">Voltar</button><a>
 
 <div>
