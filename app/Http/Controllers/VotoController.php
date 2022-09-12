fca9d4bc7a1a21323d4483b951fa21e69fd262bd
João Vitor Angelo Pereira <jvangelopereira@gmail.com>
2022-09-12 08:58:01 -0300

    verificacao de votos nulos

--- app/Http/Controllers/VotoController.php
+++ app/Http/Controllers/VotoController.php
@@ -29,66 +29,112 @@ class VotoController extends Controller
         $deputado_federal = DB::table('candidatos')->where('numero', $data['deputado_federal'])->get();
         $deputado_estadual = DB::table('candidatos')->where('numero', $data['deputado_estadual'])->get();
 
+        if (isset($presidente[0])) {
+            $presidente = $presidente[0];
+        } else {
+            $presidente = null;
+        }
+
+        if (isset($governador[0])) {
+            $governador = $governador[0];
+        } else {
+            $governador = null;
+        }
+
+        if (isset($senador[0])) {
+            $senador = $senador[0];
+        } else {
+            $senador = null;
+        }
+
+        if (isset($deputado_federal[0])) {
+            $deputado_federal = $deputado_federal[0];
+        } else {
+            $deputado_federal = null;
+        }
+
+        if (isset($deputado_estadual[0])) {
+            $deputado_estadual = $deputado_estadual[0];
+        } else {
+            $deputado_estadual = null;
+        }
+
         return view('votos.confirmar', [
-            'presidente' => $presidente[0],
-            'governador' => $governador[0],
-            'senador' => $senador[0],
-            'deputado_federal' => $deputado_federal[0],
-            'deputado_estadual' => $deputado_estadual[0],
+            'presidente' => $presidente,
+            'governador' => $governador,
+            'senador' => $senador,
+            'deputado_federal' => $deputado_federal,
+            'deputado_estadual' => $deputado_estadual,
             'zona' => $data['zona'],
             'secao' => $data['secao']
         ]);
     }
 
     function store(Request $request)
-    {   
+    {
         $data = $request->all();
         unset($data['_token']);
 
-        DB::transaction( function() use (&$data, &$request) {
-            
-            DB::table('votos')->insert([
-                'data' => Carbon::now()->format('Y-m-d H:i:s'), // ver o format
-                'candidato_id' => $data['presidente_id'],
-                'zona' => $data['zona'],
-                'secao' => $data['secao']
-            ]);
-            DB::table('votos')->insert([
-                'data' => Carbon::now()->format('Y-m-d H:i:s'),
-                'candidato_id' => $data['governador_id'],
-                'zona' => $data['zona'],
-                'secao' => $data['secao']
-            ]);
-            DB::table('votos')->insert([
-                'data' => Carbon::now()->format('Y-m-d H:i:s'),
-                'candidato_id' => $data['senador_id'],
-                'zona' => $data['zona'],
-                'secao' => $data['secao']
-            ]);
-            DB::table('votos')->insert([
-                'data' => Carbon::now()->format('Y-m-d H:i:s'),
-                'candidato_id' => $data['deputado_federal_id'],
-                'zona' => $data['zona'],
-                'secao' => $data['secao']
-            ]);
-            DB::table('votos')->insert([
-                'data' => Carbon::now()->format('Y-m-d H:i:s'),
-                'candidato_id' => $data['deputado_estadual_id'],
-                'zona' => $data['zona'],
-                'secao' => $data['secao']
-            ]);
-            $request->session()->forget('voto');
+        DB::transaction(function () use (&$data, &$request) {
 
+            if (isset($data['presidente_id'])) {
+                DB::table('votos')->insert([
+                    'data' => Carbon::now()->format('Y-m-d H:i:s'), // ver o format
+                    'candidato_id' => $data['presidente_id'],
+                    'zona' => $data['zona'],
+                    'secao' => $data['secao']
+                ]);
+            }
+
+            if (isset($data['governador_id'])) {
+                DB::table('votos')->insert([
+                    'data' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'candidato_id' => $data['governador_id'],
+                    'zona' => $data['zona'],
+                    'secao' => $data['secao']
+                ]);
+            }
+
+            if (isset($data['senador_id'])) {
+                DB::table('votos')->insert([
+                    'data' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'candidato_id' => $data['senador_id'],
+                    'zona' => $data['zona'],
+                    'secao' => $data['secao']
+                ]);
+            }
+
+            if (isset($data['deputado_federal_id'])) {
+                DB::table('votos')->insert([
+                    'data' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'candidato_id' => $data['deputado_federal_id'],
+                    'zona' => $data['zona'],
+                    'secao' => $data['secao']
+                ]);
+            }
+
+            if (isset($data['deputado_estadual_id'])) {
+                DB::table('votos')->insert([
+                    'data' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'candidato_id' => $data['deputado_estadual_id'],
+                    'zona' => $data['zona'],
+                    'secao' => $data['secao']
+                ]);
+            }
+
+            $request->session()->forget('voto');
         });
 
         return redirect('/');
     }
 
-    function titulo(){
+    function titulo()
+    {
         return view('votos.titulo');
     }
 
-    function validar(Request $request){
+    function validar(Request $request)
+    {
         $data = $request->all();
         unset($data['_token']);
 
@@ -104,21 +150,21 @@ class VotoController extends Controller
         $hoje = Carbon::now()->format('Y-m-d H:i:s');
         $pid = $eid = $erro = null;
 
-        foreach($eleitores as $e){
-            if($e->titulo == $titulo){
+        foreach ($eleitores as $e) {
+            if ($e->titulo == $titulo) {
                 $eid = $e->id;
                 $valido = true;
-                foreach($periodos as $p){
-                    if($hoje >= $p->data_inicio && $hoje <= $p->data_fim){
+                foreach ($periodos as $p) {
+                    if ($hoje >= $p->data_inicio && $hoje <= $p->data_fim) {
                         $pid = $p->id;
                         $ativo = true;
-                        foreach($votantes as $v){
-                            if($pid == $v->periodo_id && $eid == $v->eleitor_id){
+                        foreach ($votantes as $v) {
+                            if ($pid == $v->periodo_id && $eid == $v->eleitor_id) {
                                 $votou = true;
                                 break;
                             }
                         }
-                        if(!$votou){
+                        if (!$votou) {
                             $request->session()->put('voto',  true);
                             return view('/votos/create', [
                                 "zona" => $zona,
@@ -131,62 +177,61 @@ class VotoController extends Controller
                 break;
             }
         }
-        if(!$valido){
+        if (!$valido) {
             $erro = "Este título não está cadastrado!";
-        }elseif(!$ativo){
+        } elseif (!$ativo) {
             $erro = "Não há nenhum período ativo no momento!";
-        }elseif($votou){
+        } elseif ($votou) {
             $erro = "Não é possível votar mais de uma vez em um período!";
         }
         return view('/votos/titulo', ['erro' => $erro]);
-
     }
 
-    function resultados(){
-        $pgeral = $pzonas = $pzvotos = $psecoes = $psvotos =[];
+    function resultados()
+    {
+        $pgeral = $pzonas = $pzvotos = $psecoes = $psvotos = [];
         $periodos = DB::table('periodos')->orderby('data_fim', 'DESC')->get();
-        foreach($periodos as $p){
+        foreach ($periodos as $p) {
             $pgeral[$p->id] = DB::table('votos')
-            ->select('candidatos.cargo as cargo','candidatos.nome as nome', DB::raw('count(votos.candidato_id) as votos'))
-            ->leftjoin('candidatos', 'votos.candidato_id', '=', 'candidatos.id')
-            ->where([['votos.data', '<=', $p->data_fim], ['votos.data', '>=', $p->data_inicio], ['cargo', '=', 1]])
-            ->groupby('candidatos.nome', 'cargo')
-            ->orderby('votos', 'DESC')
-            ->get();
+                ->select('candidatos.cargo as cargo', 'candidatos.nome as nome', DB::raw('count(votos.candidato_id) as votos'))
+                ->leftjoin('candidatos', 'votos.candidato_id', '=', 'candidatos.id')
+                ->where([['votos.data', '<=', $p->data_fim], ['votos.data', '>=', $p->data_inicio], ['cargo', '=', 1]])
+                ->groupby('candidatos.nome', 'cargo')
+                ->orderby('votos', 'DESC')
+                ->get();
 
-            $pzonas [$p->id] = DB::table('votos')
-            ->select('candidatos.cargo as cargo','votos.zona as zona')
-            ->leftjoin('candidatos', 'votos.candidato_id', '=', 'candidatos.id')
-            ->where([['votos.data', '<=', $p->data_fim], ['votos.data', '>=', $p->data_inicio], ['cargo', '=', 1]])
-            ->groupby('zona', 'cargo')
-            ->orderby('zona', 'ASC')
-            ->get();
+            $pzonas[$p->id] = DB::table('votos')
+                ->select('candidatos.cargo as cargo', 'votos.zona as zona')
+                ->leftjoin('candidatos', 'votos.candidato_id', '=', 'candidatos.id')
+                ->where([['votos.data', '<=', $p->data_fim], ['votos.data', '>=', $p->data_inicio], ['cargo', '=', 1]])
+                ->groupby('zona', 'cargo')
+                ->orderby('zona', 'ASC')
+                ->get();
 
             $pzvotos[$p->id] = DB::table('votos')
-            ->select('votos.zona as zona', 'candidatos.nome as nome', DB::raw('count(votos.candidato_id) as votos'))
-            ->leftjoin('candidatos', 'votos.candidato_id', '=', 'candidatos.id')
-            ->where([['votos.data', '<=', $p->data_fim], ['votos.data', '>=', $p->data_inicio], ['cargo', '=', 1]])
-            ->groupby('votos.zona', 'candidatos.nome', 'cargo')
-            ->orderby('votos', 'DESC')
-            ->get();
+                ->select('votos.zona as zona', 'candidatos.nome as nome', DB::raw('count(votos.candidato_id) as votos'))
+                ->leftjoin('candidatos', 'votos.candidato_id', '=', 'candidatos.id')
+                ->where([['votos.data', '<=', $p->data_fim], ['votos.data', '>=', $p->data_inicio], ['cargo', '=', 1]])
+                ->groupby('votos.zona', 'candidatos.nome', 'cargo')
+                ->orderby('votos', 'DESC')
+                ->get();
 
-            $psecoes [$p->id] = DB::table('votos')
-            ->select('candidatos.cargo as cargo','votos.secao as secao')
-            ->leftjoin('candidatos', 'votos.candidato_id', '=', 'candidatos.id')
-            ->where([['votos.data', '<=', $p->data_fim], ['votos.data', '>=', $p->data_inicio], ['cargo', '=', 1]])
-            ->groupby('secao', 'cargo')
-            ->orderby('secao', 'ASC')
-            ->get();
+            $psecoes[$p->id] = DB::table('votos')
+                ->select('candidatos.cargo as cargo', 'votos.secao as secao')
+                ->leftjoin('candidatos', 'votos.candidato_id', '=', 'candidatos.id')
+                ->where([['votos.data', '<=', $p->data_fim], ['votos.data', '>=', $p->data_inicio], ['cargo', '=', 1]])
+                ->groupby('secao', 'cargo')
+                ->orderby('secao', 'ASC')
+                ->get();
 
             $psvotos[$p->id] = DB::table('votos')
-            ->select('votos.secao as secao', 'candidatos.nome as nome', DB::raw('count(votos.candidato_id) as votos'))
-            ->leftjoin('candidatos', 'votos.candidato_id', '=', 'candidatos.id')
-            ->where([['votos.data', '<=', $p->data_fim], ['votos.data', '>=', $p->data_inicio], ['cargo', '=', 1]])
-            ->groupby('votos.secao', 'candidatos.nome', 'cargo')
-            ->orderby('votos', 'DESC')
-            ->get();
+                ->select('votos.secao as secao', 'candidatos.nome as nome', DB::raw('count(votos.candidato_id) as votos'))
+                ->leftjoin('candidatos', 'votos.candidato_id', '=', 'candidatos.id')
+                ->where([['votos.data', '<=', $p->data_fim], ['votos.data', '>=', $p->data_inicio], ['cargo', '=', 1]])
+                ->groupby('votos.secao', 'candidatos.nome', 'cargo')
+                ->orderby('votos', 'DESC')
+                ->get();
         }
         return view('votos.resultados', ['periodos' => $periodos, 'pgeral' => $pgeral, 'pzonas' => $pzonas, 'pzvotos' => $pzvotos, 'psecoes' => $psecoes, 'psvotos' => $psvotos]);
-
     }
 }
--- resources/views/votos/confirmar.blade.php
+++ resources/views/votos/confirmar.blade.php
@@ -1,6 +1,8 @@
 @extends('base.index')
 
 <div class="mx-auto" style="width: 40%;" id="create">
+
+    @if (isset($presidente->nome))
         @include('components.field', [
             'classe' => 'mb-3',
             'type' => 'text',
@@ -10,9 +12,23 @@
             'class' => 'form-control',
             'value' => $presidente->nome,
             'onclick' => '',
-            'disabled' => 'disabled'
+            'disabled' => 'disabled',
         ])
+    @else
+        @include('components.field', [
+            'classe' => 'mb-3',
+            'type' => 'text',
+            'id' => 'presidente',
+            'name' => 'presidente',
+            'label' => 'Presidente',
+            'class' => 'form-control',
+            'value' => 'nulo',
+            'onclick' => '',
+            'disabled' => 'disabled',
+        ])
+    @endif
 
+    @if (isset($governador->nome))
         @include('components.field', [
             'classe' => 'mb-3',
             'type' => 'text',
@@ -22,9 +38,23 @@
             'class' => 'form-control',
             'value' => $governador->nome,
             'onclick' => '',
-            'disabled' => 'disabled'
+            'disabled' => 'disabled',
         ])
+    @else
+        @include('components.field', [
+            'classe' => 'mb-3',
+            'type' => 'text',
+            'id' => 'governador',
+            'name' => 'governador',
+            'label' => 'Governador',
+            'class' => 'form-control',
+            'value' => 'nulo',
+            'onclick' => '',
+            'disabled' => 'disabled',
+        ])
+    @endif
 
+    @if (isset($senador->nome))
         @include('components.field', [
             'classe' => 'mb-3',
             'type' => 'text',
@@ -34,9 +64,23 @@
             'class' => 'form-control',
             'value' => $senador->nome,
             'onclick' => '',
-            'disabled' => 'disabled'
+            'disabled' => 'disabled',
         ])
+    @else
+        @include('components.field', [
+            'classe' => 'mb-3',
+            'type' => 'text',
+            'id' => 'senador',
+            'name' => 'senador',
+            'label' => 'Senador',
+            'class' => 'form-control',
+            'value' => 'nulo',
+            'onclick' => '',
+            'disabled' => 'disabled',
+        ])
+    @endif
 
+    @if (isset($deputado_federal->nome))
         @include('components.field', [
             'classe' => 'mb-3',
             'type' => 'text',
@@ -46,9 +90,23 @@
             'class' => 'form-control',
             'value' => $deputado_federal->nome,
             'onclick' => '',
-            'disabled' => 'disabled'
+            'disabled' => 'disabled',
         ])
+    @else
+        @include('components.field', [
+            'classe' => 'mb-3',
+            'type' => 'text',
+            'id' => 'deputado_federal',
+            'name' => 'deputado_federal',
+            'label' => 'Deputado Federal',
+            'class' => 'form-control',
+            'value' => 'nulo',
+            'onclick' => '',
+            'disabled' => 'disabled',
+        ])
+    @endif
 
+    @if (isset($deputado_estadual->nome))
         @include('components.field', [
             'classe' => 'mb-3',
             'type' => 'text',
@@ -58,11 +116,25 @@
             'class' => 'form-control',
             'value' => $deputado_estadual->nome,
             'onclick' => '',
-            'disabled' => 'disabled'
+            'disabled' => 'disabled',
         ])
+    @else
+        @include('components.field', [
+            'classe' => 'mb-3',
+            'type' => 'text',
+            'id' => 'deputado_estadual',
+            'name' => 'deputado_estadual',
+            'label' => 'Deputado Estadual',
+            'class' => 'form-control',
+            'value' => 'nulo',
+            'onclick' => '',
+            'disabled' => 'disabled',
+        ])
+    @endif
+
     <form action='/votos/store' method='post'>
-    @csrf
-    @include('components.field', [
+        @csrf
+        @include('components.field', [
             'classe' => '',
             'type' => 'hidden',
             'id' => 'zona',
@@ -71,9 +143,9 @@
             'class' => '',
             'value' => $zona,
             'onclick' => '',
-            'disabled' => ''
-    ])
-    @include('components.field', [
+            'disabled' => '',
+        ])
+        @include('components.field', [
             'classe' => '',
             'type' => 'hidden',
             'id' => 'secao',
@@ -82,70 +154,145 @@
             'class' => '',
             'value' => $secao,
             'onclick' => '',
-            'disabled' => ''
+            'disabled' => '',
         ])
 
-    @include('components.field', [
-            'classe' => '',
-            'type' => 'hidden',
-            'id' => 'presidente_id',
-            'name' => 'presidente_id',
-            'label' => '',
-            'class' => '',
-            'value' => $presidente->id,
-            'onclick' => '',
-            'disabled' => ''
-        ])
+        @if (isset($presidente->id))
+            @include('components.field', [
+                'classe' => '',
+                'type' => 'hidden',
+                'id' => 'presidente_id',
+                'name' => 'presidente_id',
+                'label' => '',
+                'class' => '',
+                'value' => $presidente->id,
+                'onclick' => '',
+                'disabled' => '',
+            ])
+    {{--     @else
+            @include('components.field', [
+                'classe' => '',
+                'type' => 'hidden',
+                'id' => 'presidente_id',
+                'name' => 'presidente_id',
+                'label' => '',
+                'class' => '',
+                'value' => null,
+                'onclick' => '',
+                'disabled' => '',
+            ]) --}}
+        @endif
 
-        @include('components.field', [
-            'classe' => '',
-            'type' => 'hidden',
-            'id' => 'governador_id',
-            'name' => 'governador_id',
-            'label' => '',
-            'class' => '',
-            'value' => $governador->id,
-            'onclick' => '',
-            'disabled' => ''
-        ])
+        @if (isset($governador->id))
+            @include('components.field', [
+                'classe' => '',
+                'type' => 'hidden',
+                'id' => 'governador_id',
+                'name' => 'governador_id',
+                'label' => '',
+                'class' => '',
+                'value' => $governador->id,
+                'onclick' => '',
+                'disabled' => '',
+            ])
+{{--         @else
+            @include('components.field', [
+                'classe' => '',
+                'type' => 'hidden',
+                'id' => 'governador_id',
+                'name' => 'governador_id',
+                'label' => '',
+                'class' => '',
+                'value' => null,
+                'onclick' => '',
+                'disabled' => '',
+            ]) --}}
+        @endif
 
-        @include('components.field', [
-            'classe' => '',
-            'type' => 'hidden',
-            'id' => 'senador_id',
-            'name' => 'senador_id',
-            'label' => '',
-            'class' => '',
-            'value' => $senador->id,
-            'onclick' => '',
-            'disabled' => ''
-        ])
+        @if (isset($senador->id))
+            @include('components.field', [
+                'classe' => '',
+                'type' => 'hidden',
+                'id' => 'senador_id',
+                'name' => 'senador_id',
+                'label' => '',
+                'class' => '',
+                'value' => $senador->id,
+                'onclick' => '',
+                'disabled' => '',
+            ])
+{{--         @else
+            @include('components.field', [
+                'classe' => '',
+                'type' => 'hidden',
+                'id' => 'senador_id',
+                'name' => 'senador_id',
+                'label' => '',
+                'class' => '',
+                'value' => null,
+                'onclick' => '',
+                'disabled' => '',
+            ]) --}}
+        @endif
 
-        @include('components.field', [
-            'classe' => '',
-            'type' => 'hidden',
-            'id' => 'deputado_federal_id',
-            'name' => 'deputado_federal_id',
-            'label' => '',
-            'class' => '',
-            'value' => $deputado_federal->id,
-            'onclick' => '',
-            'disabled' => ''
-        ])
+        @if (isset($deputado_federal->id))
+            @include('components.field', [
+                'classe' => '',
+                'type' => 'hidden',
+                'id' => 'deputado_federal_id',
+                'name' => 'deputado_federal_id',
+                'label' => '',
+                'class' => '',
+                'value' => $deputado_federal->id,
+                'onclick' => '',
+                'disabled' => '',
+            ])
+{{--         @else
+            @include('components.field', [
+                'classe' => '',
+                'type' => 'hidden',
+                'id' => 'deputado_federal_id',
+                'name' => 'deputado_federal_id',
+                'label' => '',
+                'class' => '',
+                'value' => null,
+                'onclick' => '',
+                'disabled' => '',
+            ]) --}}
+        @endif
 
-        @include('components.field', [
-            'classe' => '',
-            'type' => 'hidden',
-            'id' => 'deputado_estadual_id',
-            'name' => 'deputado_estadual_id',
-            'label' => '',
-            'class' => '',
-            'value' => $deputado_estadual->id,
-            'onclick' => '',
-            'disabled' => ''
-        ])
+        @if (isset($deputado_estadual->id))
+            @include('components.field', [
+                'classe' => '',
+                'type' => 'hidden',
+                'id' => 'deputado_estadual_id',
+                'name' => 'deputado_estadual_id',
+                'label' => '',
+                'class' => '',
+                'value' => $deputado_estadual->id,
+                'onclick' => '',
+                'disabled' => '',
+            ])
+        {{-- @else
+            @include('components.field', [
+                'classe' => '',
+                'type' => 'hidden',
+                'id' => 'deputado_estadual_id',
+                'name' => 'deputado_estadual_id',
+                'label' => '',
+                'class' => '',
+                'value' => null,
+                'onclick' => '',
+                'disabled' => '',
+            ]) --}}
+        @endif
 
-        @include('components.button', ['type' => 'submit', 'value' => '','color' => 'success', 'text' => 'Confirmar'])
+        @include('components.button', [
+            'type' => 'submit',
+            'value' => '',
+            'color' => 'success',
+            'text' => 'Confirmar',
+        ])
     </form>
     <a href="/votos/create" class="btn btn-danger">Voltar</a>
 </div>
